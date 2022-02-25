5209941ac0db01dc3d78b2b5c771b405d2a6d415
Landon Cuff <landoncuff23@gmail.com>
2022-02-25 11:29:21 -0700

    Lecture Part 1

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,26 @@
+//todo: 1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions) 2. Log their values to the console
+
+let country = 'Untied States of Americana';
+let continent = 'North America';
+let population = '100,000,000';
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+
+//Todo: Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable'language', but don't assign it any value yet 2. Log the types of 'isIsland', 'population', 'country' and 'language'to the console
+
+let isIsland = true;
+
+let language;
+
+console.log(typeof language);
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof population);
+
+
+// Todo: . Set the value of 'language' to the language spoken where you live (some countries have multiple languages, but just choose one) 2. Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const. 3. Try to change one of the changed variables now, and observe what happens
+
+language = 'English';
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,52 @@
+
+/* Values and variables
+
+let js = 'amazing';
+if(js === 'amazing') alert('JavaScript is Fun');
+
+console.log(40 + 8 + 23 - 10);
+
+
+! value is a pice of information 
+* storing variables (Like a box)
+let firstName = 'Landon';
+
+console.log(firstName);
+
+!Variable naming convention
+//* camelCase firstName
+//* can not write let 3years = 3
+
+*/
+
+/* Data types leature 
+
+! Data types:
+//* Primtive Data types:
+ number let age = 23
+string let name = 'Landon'
+ boolean let fullAge = true;
+undefined let children;
+null let children = null;
+ symbol
+ bigInteger another type for numbers
+//* the value has the type not variable
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'landon');
+
+dynamic typing
+changing the value inside the variable
+javascriptIsFun = 'YES!';
+
+ undefined (empty value)
+let year;
+*/
+
+
+
