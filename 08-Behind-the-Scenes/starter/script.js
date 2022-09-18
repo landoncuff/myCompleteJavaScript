ac96331c06d8ecf88b3d33d8560f755256997392
Landon Cuff <landoncuff23@gmail.com>
2022-09-18 13:02:18 -0600

    Regular Functions vs. Arrow Functions this keyword

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -99,14 +99,14 @@ const z = 12;
 
 /* 
     todo: this keyword 
-*/
-//? Displays the window object which is the this in the global scope
+
+? Displays the window object which is the this in the global scope
 console.log(this);
 
 const calcAge = function (birthYear) {
 console.log(2037 - birthYear);
-//? Would be undefined 
-//? Has no owner
+? Would be undefined 
+? Has no owner
 console.log(this); 
 }
 
@@ -115,8 +115,8 @@ calcAge(1991);
 
 const calcAgeArrow = (brithYear) => {
     console.log(2027 - brithYear);
-    //? displays the window object because arrow functions dont get their own this keyword
-    //? using the this keyword of the parent scope (global) which is window
+    ? displays the window object because arrow functions dont get their own this keyword
+    ? using the this keyword of the parent scope (global) which is window
     console.log(this);
 }
 
@@ -126,14 +126,14 @@ const calcAgeArrow = (brithYear) => {
 const landon = {
     year: 2000,
     calcAge: function () {
-        //? gets the year of the landon object
+        ? gets the year of the landon object
         console.log(2033 - this.year);
-        //? Displays the whole landon object
+        ? Displays the whole landon object
         console.log(this);
     }
 }
 
-//? instead of written landon.calcAge(1991) we can write 
+? instead of written landon.calcAge(1991) we can write 
 landon.calcAge();
 
 
@@ -141,14 +141,95 @@ const robert = {
     year: 2017
 }
 
-//? Method barrowing which we are taking the function from one object and putting it in another
+? Method barrowing which we are taking the function from one object and putting it in another
 robert.calcAge = landon.calcAge;
-//? Always points to the object that is calling the method (robert is calling)
+? Always points to the object that is calling the method (robert is calling)
 robert.calcAge();
 
 
 const f = landon.calcAge;
 
-//! throws error because year does not exits in f 
+! throws error because year does not exits in f 
 f();
 
+*/
+
+/*
+todo: Regular functions vs. Arrow functions 
+
+
+! will add firstName to the window object (DONT USE VAR)
+// var firstName = 'Landon';
+! Not a code block so it doesnt have its own scope so it is all still inside the global scope
+const landon = {
+    firstName: 'Robert',
+    year: 2000,
+    calcAge: function () {
+        console.log(2033 - this.year);
+        console.log(this);
+    },
+    ? Doesnt work because Arrow functions dont have their own 'their' keyword
+    ? Logs 'Hey undefined' becasue their is no 'firstName' in the window object
+    ? Because it doesnt have its own, it will take the 'this' keyword from the parent scope which is the global scope which is the window object(has not firstName object)
+    greet: () => console.log(`Hey ${this.firstName}`)
+}
+
+landon.greet();
+
+
+todo: function inside a method
+
+? Solution 1
+const kassidy = {
+    firstName: 'Robert',
+    year: 1996,
+    calcAge: function () {
+        console.log(2033 - this.year);
+        console.log(this);
+        ? Function to return is they are a millenial 
+        ! doesnt work because it is like if this function is called outside of the method
+        ? Working around issue (Can be called self or that)
+        const self = this;
+        const isMillenial = function () {
+            console.log(self);
+            console.log(self.year >= 1981 && self.year <= 1996);
+        }
+        isMillenial();
+    }
+}
+
+kassidy.calcAge();
+
+
+? Solution 2
+const rob = {
+    firstName: 'Robert',
+    year: 1996,
+    calcAge: function () {
+        console.log(2033 - this.year);
+        console.log(this);
+        ? Works without having to set a variable becuase arrow doesnt have its own this keyword and will use the this keyword from its parent
+        const isMillenial = () => {
+            console.log(this);
+            console.log(this.year >= 1981 && this.year <= 1996);
+        }
+        isMillenial();
+    }
+}
+
+rob.calcAge();
+
+todo: Arguments keyword
+const addExpr = function (a, b) {
+    ? Displays all the parameters
+    console.log(arguments);
+    return a + b;
+}
+addExpr(2,5);
+
+var addArrow = (a, b) => {
+    console.log(arguments);
+   return a + b;
+}
+addArrow(2,5)
+*/
