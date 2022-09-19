d69f629b6f47904d1ca9f492b53b8a9568fadd2f
Landon Cuff <landoncuff23@gmail.com>
2022-09-19 08:56:00 -0600

    Destructuring Arrays

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -11,6 +11,11 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function(starterIndex, mainIndex){
+    //? returning an array of food ordered by the customer
+    //? This will point to the given scope
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 
   openingHours: {
     thu: {
@@ -27,3 +32,72 @@ const restaurant = {
     },
   },
 };
+
+/* 
+TODO: Array destructuring
+
+
+const simp = [2,3,4];
+const a = simp[0];
+const b = simp[1];
+const c = simp[2];
+
+? Destructuring
+? Not desptroying the array, just pull data out
+! Will take from the array in order of index
+? Same as code above
+const [x,y,z] = simp;
+
+console.log(a,b,c);
+console.log(x,y,z);
+
+todo: Taking out values from the restaurant array
+const [first, second] = restaurant.categories;
+console.log(first, second);
+
+! if you only want the first and third values from the array
+? The empty space will skip over the index that is next so in this case it will skip over index 1
+const [firstItem, ,thirdItem] = restaurant.categories;
+console.log(firstItem, thirdItem);
+
+let [main, ,secondary] = restaurant.categories;
+console.log(main, secondary);
+
+? without destructuring
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+
+? Using destructuring to change the values or swap the values
+[main, secondary] = [secondary, main];
+console.log(main,secondary);
+
+todo: Function to order food
+? getting index 2 from the starter menu and getting the first index from the main menu
+? Garlic Bread & Pizza will be our return
+console.log(restaurant.order(2, 0));
+
+? Creating two variables from the return of a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+todo: Destructuring a nested array (Array within an array)
+const nested = [2, 4, [5,6]];
+? Getting the first value and the whole nested array ([5,6])
+const [value1, ,value2] = nested;
+console.log(value1,value2);
+
+? Getting the first value and all the values from the nested array doing a destructing inside a destructuring
+? Destructuring an array within an array
+const [i, ,[j, k]] = nested;
+console.log(i, j, k);
+
+todo: Setting default values when destructuring
+const [p, q, r] = [8,9];
+!Last value will be undefined
+? Default values
+? Will set a default so it is not undefined
+const [w=1, m=1, n=1] = [8,9];
+console.log(w, m, n);
+*/
+
