b7bebc2eab5e5f6ac24bbb4e2bfa1f5cfa7fdccb
Landon Cuff <landoncuff23@gmail.com>
2022-09-28 08:51:06 -0600

    Coding Challenge #1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -340,3 +340,114 @@ rest2.owner &&= '<ANONYMOUS>';
 ! doest not return undefined because it did not exist
 rest1.owner &&= '<ANONYMOUS>';
 */
+
+/* 
+TODO: Coding Challenge #1
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+* 1. Create one player array for each team (variables 'players1' and 'players2')
+* 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+* 3. Create an array 'allPlayers' containing all players of both teams (22 players)
+* 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+* 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+* 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+* 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+
+
+const game = {
+  team1: 'Bayern Munich', 
+  team2: 'Borrussia Dortmund', 
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ], 
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ], 
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+  'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  }, 
+};
+
+! Step 1
+* const [players1, players2] = game.players;
+const players1 = game.players[0];
+const players2 = game.players[1];
+
+console.log(players1);
+console.log(players2);
+
+! Step 2
+? creating one variable to hold the value of the first player from the array and then adding the rest of the players to the fieldPlayers array
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+! Step 3
+? Creating a new array to hold all the players from both teams and using the spead operator to break both arrays apart into one
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+! Step 4
+? Creating a new array to hold 3 new players. Breaking apart the first array and then adding the 3 new players
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+! Step 5
+? Destructuring an object and putting them into new variables. Need to spesify the name. Had to call x then create the new variable name (x: draw). Draw is the new variable and x is the key
+* const {odds: {team1, x: draw, team2}} = game;
+const {team1, x: draw, team2} = game.odds;
+console.log(team1, draw, team2);
+
+! Step 6 
+? Creates a function that takes an arbitary number of players (Takes a comma sperated string and puts it into an array)
+const printGoals = function(...players){
+  * console.log(`${players.length}`);
+  let sum = 0;
+  for(let i = 0; i < players.length; i++){
+    sum++;
+  }
+  console.log(sum);
+}
+
+? Spreading the array to be a comma seperated string
+printGoals(...players1);
+printGoals(...allPlayers);
+
+! Step 7
+? Using logical operators to determine if the the condition is true. Need to use && because we need to the first condition to be true and not false
+team1 < team2 && console.log('Team 1 is more likley to wiin');
+team1 > team2 && console.log('Team 2 is more likley to wiin');
+*/
