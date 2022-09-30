c735fa7c3ad73e4e1f75d560b7db527cff0eb4dc
Landon Cuff <landoncuff23@gmail.com>
2022-09-30 09:21:32 -0600

    Object Literals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -5,6 +5,7 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+/*
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -45,6 +46,7 @@ const restaurant = {
     },
   },
 };
+*/
 
 /* 
 TODO: Array destructuring
@@ -481,3 +483,64 @@ for(const [i, el] of menu.entries()){
   console.log(`${i +1}: ${el}`);
 }
 */
+
+/*
+TODO: Enchanced Object Literals
+*/
+
+//? Object literals is "Literally" writing an object in your code
+//? ES6 has three new ways to make it easier to write object literals
+    //? The first way is to call another object inside an object. Placing the values within the current object
+    //? The second way to call a method inside an object
+    //? You can now compute inside the object itself (Add things together or change the name of key to be able to compute)
+
+//* Compute the weekdays
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+//? Copying the resturant object
+//* Removing the 'openingHours' object inside the resturant object to use an easier method (First way)
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function(starterIndex, mainIndex){
+    //? returning an array of food ordered by the customer
+    //? This will point to the given scope
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  //! New ES6 you dont need to write the "function" keyword
+  orderDelivery({starterIndex = 1, mainIndex = 0, time = '5:00', address = 'No Address Found'}){
+    console.log(`Order Received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time} `);
+  },
+  orderPasta(ing1, ing2, ing3){
+    console.log(`Here is your pasta with ${ing1} and ${ing2} and ${ing3}`);
+  },
+  orderPizza(mainIngredient, ...otherIngredients){
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+  //! Old way to add openingHours back to the restaurant object
+  // openingHours: openingHours,
+  //! New ES6 way to use object literals
+  openingHours,
+};
+
+console.log(restaurant);
