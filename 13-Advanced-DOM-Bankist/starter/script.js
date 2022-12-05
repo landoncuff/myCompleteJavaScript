34208d3499491cc0da919326fe25aec0fdeb9d7d
Landon Cuff <landoncuff23@gmail.com>
2022-12-05 08:53:32 -0700

    Selecting, creating, and deleting DOM elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -33,3 +33,112 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+/*
+TODO: Selecting, Creating, and Deleting Elements
+
+////////////////////////////////////////// SELECTING ELEMENTS: ///////////////////////////////////////////////////
+
+console.log(document.documentElement); // Whole HTML page
+console.log(document.head); // Head of HTML page
+console.log(document.body); // Body of HTML page
+
+// Gets first instance of Header class
+document.querySelector('.header');
+// Gets ALL instances of Section class
+const allSections = document.querySelectorAll('.section');
+
+document.getElementById('section--1');
+
+// Returns HTML collection (LIVE collection) -- Updates when DOM updates
+const allButtons = document.getElementsByTagName('button');
+
+const allClasses = document.getElementsByClassName('btn');
+
+
+//////////////////////////////////////// Creating & Inserting Elements: ////////////////////////////////////////////
+
+// Creating:
+const message = document.createElement('div');
+// because message is a DOM object, we can use methods (adding a class)
+message.classList.add('cookie-message');
+// Adding text to the div
+message.textContent = 'We use cookies for improved functionality and analytics.'
+// adding HTML to the div
+message.innerHTML = 'We use cookies for improved functionality and analytics.<button ' +
+  'class="btn btn--close-cookie">Got it!</button>';
+
+
+const header = document.querySelector('.header');
+// Adding the object to the HTML page as the FIRST child of the header element
+header.prepend(message);
+// Adding the object as the LAST child of the header element
+header.append(message);
+// Allows you to display both elements at once
+header.append(message.cloneNode(true));
+// Displays the message before header element
+header.before(message);
+// Displays the message after header element
+header.after(message);
+
+/////////////////////////////////////// DELETE ELEMENTS: /////////////////////////////////////////////////////////
+
+document.querySelector('.btn--close-cookie').addEventListener('click', function (){
+  message.remove();
+})
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
