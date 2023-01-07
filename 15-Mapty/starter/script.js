42a421f10bc267fe1d30c7caa0af7a1c921b17e6
Landon Cuff <landoncuff23@gmail.com>
2023-01-07 10:55:15 -0700

    Geolocation API & Leaflet Mpas

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,31 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+
+/*
+TODO: Geolocation API
+ */
+
+// Current Google Maps URL https://www.google.com/maps/@40.3623163,-111.8666752,14z
+navigator.geolocation.getCurrentPosition(function (position){
+  // using destructuring to put values into an object
+  const {latitude} = position.coords;
+  const {longitude} = position.coords;
+  console.log(`https://www.google.com/maps/@${latitude},${longitude},14z`);
+
+  const coords = [latitude, longitude];
+
+  const map = L.map('map').setView(coords, 13); // Second param is the zoom
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  L.marker(coords).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+}, function (){
+  alert('Could not get your current location');
+});
