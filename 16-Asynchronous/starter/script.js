7395ce5820a0203b17317f06a7242b508d020efb
Landon Cuff <landoncuff23@gmail.com>
2023-01-18 16:24:57 -0700

    Promises, Fetch API and Consuming Promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -46,10 +46,8 @@ getCountryData('portugal');
 
  */
 
-
 /*
 TODO: AJAX callback sequence (Callback Hell)
- */
 
 
 const renderCountry = function (data, className = ""){
@@ -100,11 +98,48 @@ const getCountryDataAndNeighbour = function (country) {
 getCountryDataAndNeighbour('usa');
 // getCountryDataAndNeighbour('portugal');
 
+ */
 
+/*
+TODO: Promises and the Fetch API
 
+// Instead of the AJAX call from other examples, we use the fetch method
+const request = fetch('https://restcountries.com/v2/name/usa');
+console.log(request); // Returns a Promise
 
+ */
 
+/*
+TODO: Consuming Promises
+ */
 
+const renderCountry = function (data, className = ""){
+  const html = `
+        <article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+          </div>
+        </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+}
+
+const getCountryData = function (country){
+  const request = fetch(`https://restcountries.com/v2/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => renderCountry(data[0])
+  );
+}
+
+getCountryData('usa');
+getCountryData('portugal');
 
 
 
