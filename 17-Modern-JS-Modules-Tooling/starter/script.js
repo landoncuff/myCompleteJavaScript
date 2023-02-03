57d41edf99f42f689f119997b5d7b8b4477169db
Landon Cuff <landoncuff23@gmail.com>
2023-02-03 16:03:59 -0700

    Practicing Imports & Exports

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,3 +1,19 @@
-/*
-No Code.. Just videos
- */
+// Importing values from other module
+import {addToChart, totalQunatity, totalPrice as price} from "./shoppingCart.js";
+import * as CartShopping from './shoppingCart.js';
+import AddProduct from './shoppingCart.js';
+
+
+console.log('Importing Module')
+
+// calling the function that is defined inside the shoppingChart module
+addToChart('Protein Powder', 3);
+console.log(price, totalQunatity);
+
+// Using the import all
+CartShopping.addToChart('Protein', 3);
+console.log(CartShopping.tq);
+
+
+// Getting values from default
+AddProduct('Protein', 5);
