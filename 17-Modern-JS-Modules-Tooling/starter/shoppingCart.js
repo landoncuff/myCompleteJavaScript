5158e6f5b775fd50ff11031b7e5d94cd0bd17a1c
Landon Cuff <landoncuff23@gmail.com>
2023-02-03 16:04:16 -0700

    Practicing Imports & Exports

---
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -0,0 +1,21 @@
+console.log('Exporting Module');
+
+const shippingCost = 10;
+const cart = [];
+
+// Exporting this function (Named Export)
+export const addToChart = function (product, qunatity){
+  cart.push({product, qunatity});
+  console.log(`${qunatity} ${product} added to chart`);
+}
+
+const totalPrice = 237;
+const totalQunatity = 34;
+
+export {totalPrice, totalQunatity as tq};
+
+// Exporting only one thing per module
+export default function (product, quantity){
+  cart.push({product, quantity});
+  console.log(`${quantity} ${product} added to chart`);
+}
