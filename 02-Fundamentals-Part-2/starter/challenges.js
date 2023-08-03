0e57e3ce2b582e270e933f0b6bcd0b29e95ae41a
Landon Cuff <landoncuff23@gmail.com>
2023-08-03 17:29:23 -0600

    Challenge 1-5

--- 02-Fundamentals-Part-2/starter/challenges.js
+++ 02-Fundamentals-Part-2/starter/challenges.js
@@ -1,6 +1,11 @@
-//! Challenge 1: 
+// Did Challenge Again
 
-// TODO: Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently. Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team). A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
+//! Challenge 1:
+
+// TODO: Back to the two gymnastics teams, the Dolphins and the Koalas!
+//  There is a new gymnastics discipline, which works differently.
+//  Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+//  A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
 
 //* Your tasks: 1. Createanarrowfunction'calcAverage'tocalculatetheaverageof3scores 2. Usethefunctiontocalculatetheaverageforbothteams 3. Createafunction'checkWinner'thattakestheaveragescoreofeachteam as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)" 4. Usethe'checkWinner'functiontodeterminethewinnerforbothData1and Data 2 5. Ignoredrawsthistime Test data: § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49 § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27 Hints: § To calculate average of 3 values, add them all together and divide by 3 § To check if number A is at least double number B, check for A >= 2 * B. Apply this to the team's average scores 😉
 
